89a33fe424c5bae537f797a8b39abb12e9c9b8ec
csp <chenshaoping@52tt.com>
2021-02-28 16:19:22 +0800

    提交登录页面

---
+++ lib/business/login/login_model.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_model.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+class _LoginModel implements LoginModel {
+  _LoginModel(this._dio, {this.baseUrl}) {
+    ArgumentError.checkNotNull(_dio, '_dio');
+    baseUrl ??= 'https://www.wanandroid.com/';
+  }
+
+  final Dio _dio;
+
+  String baseUrl;
+
+  @override
+  Future<LoginResult> login(username, password) async {
+    ArgumentError.checkNotNull(username, 'username');
+    ArgumentError.checkNotNull(password, 'password');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = {'username': username, 'password': password};
+    _data.removeWhere((k, v) => v == null);
+    final _result = await _dio.request<Map<String, dynamic>>('user/login',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'POST',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            contentType: 'application/x-www-form-urlencoded',
+            baseUrl: baseUrl),
+        data: _data);
+    final value = LoginResult.fromJson(_result.data);
+    return value;
+  }
+}
---
+++ lib/business/login/model/login_result.g.dart
@@ -0,0 +1,58 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_result.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LoginResult _$LoginResultFromJson(Map<String, dynamic> json) {
+  return LoginResult(
+    json['data'] == null
+        ? null
+        : Data.fromJson(json['data'] as Map<String, dynamic>),
+    json['errorCode'] as int,
+    json['errorMsg'] as String,
+  );
+}
+
+Map<String, dynamic> _$LoginResultToJson(LoginResult instance) =>
+    <String, dynamic>{
+      'data': instance.data,
+      'errorCode': instance.errorCode,
+      'errorMsg': instance.errorMsg,
+    };
+
+Data _$DataFromJson(Map<String, dynamic> json) {
+  return Data(
+    json['admin'] as bool,
+    json['chapterTops'] as List,
+    json['coinCount'] as int,
+    json['collectIds'] as List,
+    json['email'] as String,
+    json['icon'] as String,
+    json['id'] as int,
+    json['nickname'] as String,
+    json['password'] as String,
+    json['publicName'] as String,
+    json['token'] as String,
+    json['type'] as int,
+    json['username'] as String,
+  );
+}
+
+Map<String, dynamic> _$DataToJson(Data instance) => <String, dynamic>{
+      'admin': instance.admin,
+      'chapterTops': instance.chapterTops,
+      'coinCount': instance.coinCount,
+      'collectIds': instance.collectIds,
+      'email': instance.email,
+      'icon': instance.icon,
+      'id': instance.id,
+      'nickname': instance.nickname,
+      'password': instance.password,
+      'publicName': instance.publicName,
+      'token': instance.token,
+      'type': instance.type,
+      'username': instance.username,
+    };
