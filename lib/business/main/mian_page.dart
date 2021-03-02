a857bb1699ce676c668a1f47d9bab57908444f2e
csp <chenshaoping@52tt.com>
2021-03-02 23:15:00 +0800

    修改类名

--- lib/business/main/mian_page.dart
+++ lib/business/main/mian_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:wan_android_flutter/base/base_state.dart';
 import 'package:wan_android_flutter/base/base_stateful_widget.dart';
-import 'package:wan_android_flutter/base/util/system_util.dart';
+import 'package:wan_android_flutter/base/util/screen_util.dart';
 import 'package:wan_android_flutter/business/main/main_view_model.dart';
 
 class MainPage extends BaseStatefulWidget {
@@ -20,7 +20,7 @@ class MainState extends BaseState<MainPage, MainViewModel> {
       home: Scaffold(
         body: Container(
           alignment: Alignment.topCenter,
-          padding: EdgeInsets.only(top: SystemUtil.getStatusBarHigh()),
+          padding: EdgeInsets.only(top: ScreenUtil.getStatusBarHigh()),
           child: Text("ada"),
         ),
         // bottomNavigationBar: BottomNavigationBar(
