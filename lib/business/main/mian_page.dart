8135e52bf1f73f95510a6d41c96a6bb6a7494cd1
陈少平 <chenshaoping@52tt.com>
2021-03-04 09:39:03 +0800

    提交MainPage

--- lib/business/main/mian_page.dart
+++ lib/business/main/mian_page.dart
@@ -8,11 +8,9 @@ import 'package:wan_android_flutter/business/main/main_view_model.dart';
 class MainPage extends BaseStatefulWidget {
   @override
   State<StatefulWidget> createState() => MainState();
-
 }
 
 class MainState extends BaseState<MainPage, MainViewModel> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,12 +21,39 @@ class MainState extends BaseState<MainPage, MainViewModel> {
           padding: EdgeInsets.only(top: ScreenUtil.getStatusBarHigh()),
           child: Text("ada"),
         ),
-        // bottomNavigationBar: BottomNavigationBar(
-        //
-        // ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: viewModel.currPageIndex,
+          onTap: (index) {
+            viewModel.currPageIndex = index;
+            setState(() {});
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: getIcon(Icons.home),
+                activeIcon: getActiveIcon(Icons.home),
+                title: getBottomText('首页')),
+            BottomNavigationBarItem(
+                icon: getIcon(Icons.list),
+                activeIcon: getActiveIcon(Icons.list),
+                title: getBottomText('公众号')),
+            BottomNavigationBarItem(
+                icon: getIcon(Icons.map),
+                activeIcon: getActiveIcon(Icons.map),
+                title: getBottomText('知识体系')),
+            BottomNavigationBarItem(
+                icon: getIcon(Icons.description),
+                activeIcon: getActiveIcon(Icons.description),
+                title: getBottomText('项目'))
+          ],
+        ),
       ),
-
     );
   }
 
+  Icon getIcon(IconData iconData) => Icon(iconData, color: Colors.grey);
+
+  Icon getActiveIcon(IconData iconData) => Icon(iconData, color: Colors.blue);
+
+  Text getBottomText(String text) =>
+      Text(text, style: TextStyle(color: Colors.blue));
 }
