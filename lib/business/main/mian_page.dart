62bbc03ee7632a693c97600dcf33281efa495cda
陈少平 <chenshaoping@52tt.com>
2021-03-15 18:26:21 +0800

    解决首页切换导致page重新加载的问题

--- lib/business/main/mian_page.dart
+++ lib/business/main/mian_page.dart
@@ -16,17 +16,22 @@ class MainPage extends BaseStatefulWidget {
 }
 
 class MainState extends BaseState<MainPage, MainViewModel> {
-  HomePage _homePage;
-  OfficialAccountsPage _officialAccountsPage;
-  KnowledgePage _knowledgePage;
-  ProjectPage _projectPage;
+  List<Widget> _pageList = [
+    HomePage(),
+    OfficialAccountsPage(),
+    KnowledgePage(),
+    ProjectPage()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: getPage(viewModel.currPageIndex),
+        body: IndexedStack(
+          index: viewModel.currPageIndex,
+          children: _pageList,
+        ),
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: viewModel.currPageIndex,
           onTap: (index) {
@@ -37,7 +42,7 @@ class MainState extends BaseState<MainPage, MainViewModel> {
             BottomNavigationBarItem(
                 icon: getIcon(Icons.home),
                 activeIcon: getActiveIcon(Icons.home),
-                title: getBottomText("首页")),//退出后在进入到这里的时候国际化回报错
+                title: getBottomText("首页")), //退出后在进入到这里的时候国际化回报错
             BottomNavigationBarItem(
                 icon: getIcon(Icons.list),
                 activeIcon: getActiveIcon(Icons.list),
@@ -56,42 +61,11 @@ class MainState extends BaseState<MainPage, MainViewModel> {
     );
   }
 
-  // ignore: missing_return
-  BaseStatefulWidget getPage(int currIndex) {
-    switch (currIndex) {
-      case 0:
-        if (_homePage == null) {
-          _homePage = HomePage();
-        }
-        return _homePage;
-        break;
-      case 1:
-        if (_officialAccountsPage == null) {
-          _officialAccountsPage = OfficialAccountsPage();
-        }
-        return _officialAccountsPage;
-        break;
-      case 2:
-        if (_knowledgePage == null) {
-          _knowledgePage = KnowledgePage();
-        }
-        return _knowledgePage;
-        break;
-      case 3:
-        if (_projectPage == null) {
-          _projectPage = ProjectPage();
-        }
-        return _projectPage;
-        break;
-      default:
-        break;
-    }
-  }
-
   Icon getIcon(IconData iconData) => Icon(iconData, color: Colors.grey);
 
-  Icon getActiveIcon(IconData iconData) => Icon(iconData, color: Color(0xFA000000));
+  Icon getActiveIcon(IconData iconData) =>
+      Icon(iconData, color: Color(0xFA000000));
 
   Text getBottomText(String text) =>
-      Text(text, style: TextStyle(color:  Color(0xFA000000)));
+      Text(text, style: TextStyle(color: Color(0xFA000000)));
 }
