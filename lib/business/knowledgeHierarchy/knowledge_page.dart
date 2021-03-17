c2762971ead83f5993aa04e551b95f70f414d764
csp <chenshaoping@52tt.com>
2021-03-17 23:25:51 +0800

    知识体系添加搜索按钮

--- lib/business/knowledgeHierarchy/knowledge_page.dart
+++ lib/business/knowledgeHierarchy/knowledge_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:wan_android_flutter/base/base_state.dart';
 import 'package:wan_android_flutter/base/base_stateful_widget.dart';
 import 'package:wan_android_flutter/business/knowledgeHierarchy/knowledge_view_model.dart';
+import 'package:wan_android_flutter/business/search/search_page.dart';
 import 'package:wan_android_flutter/const/resource.dart';
 
 class KnowledgePage extends BaseStatefulWidget {
@@ -20,40 +21,26 @@ class KnowledgeState extends BaseState<KnowledgePage, KnowledgeViewModel> {
       home: Scaffold(
         body: CustomScrollView(
           slivers: <Widget>[
-            const SliverAppBar(
+            SliverAppBar(
               backgroundColor: Colors.black87,
               pinned: true,
               expandedHeight: 250.0,
-              leading: SizedBox(
-                width: 26,
-                height: 26,
-                child: Image(
-                    fit: BoxFit.fill,
-                    width: 26,
-                    height: 26,
-                    image: AssetImage(
-                        R.ASSETS_IMAGES_BASELINE_SEARCH_WHITE_24DP_PNG)),
+              leading: GestureDetector(
+                onTap: (){
+                  push(SearchPage());
+                },
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  width: 26,
+                  height: 26,
+                  child: Image(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                          R.ASSETS_IMAGES_BASELINE_SEARCH_WHITE_24DP_PNG)),
+                ),
               ),
               flexibleSpace: const FlexibleSpaceBar(
                 title: const Text('知识体系'),
-              ),
-            ),
-            new SliverGrid(
-              gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200.0,
-                mainAxisSpacing: 10.0,
-                crossAxisSpacing: 10.0,
-                childAspectRatio: 4.0,
-              ),
-              delegate: new SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  return new Container(
-                    alignment: Alignment.center,
-                    color: Colors.teal[100 * (index % 9)],
-                    child: new Text('grid item $index'),
-                  );
-                },
-                childCount: 20,
               ),
             ),
             new SliverFixedExtentList(
@@ -65,7 +52,7 @@ class KnowledgeState extends BaseState<KnowledgePage, KnowledgeViewModel> {
                   color: Colors.lightBlue[100 * (index % 9)],
                   child: new Text('list item $index'),
                 );
-              }, childCount: 10),
+              }, childCount: 20),
             ),
           ],
         ),
