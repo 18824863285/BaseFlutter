f597411e881ca3d468d8205ba3b77bf0fe712515
陈少平 <chenshaoping@52tt.com>
2021-07-30 10:28:03 +0800

    添加网络权限，修复release情况下不能做网络请求的bug

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -7,7 +7,7 @@
 // ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
@@ -19,43 +19,46 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en';
 
-  static m0(time) => "剩余${time}S";
+  static String m0(time) => "剩余${time}S";
 
-  static m1(name) => "language ${name}";
+  static String m1(name) => "language ${name}";
 
-  static m2(param1, param2) => "${param1} , s ${param2}";
+  static String m2(param1, param2) => "${param1} , s ${param2}";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static _notInlinedMessages(_) => <String, Function> {
-    "account" : MessageLookupByLibrary.simpleMessage("账号"),
-    "age" : MessageLookupByLibrary.simpleMessage("age"),
-    "ensure_psw" : MessageLookupByLibrary.simpleMessage("确认密码"),
-    "ensure_psw_true" : MessageLookupByLibrary.simpleMessage("密码和确认密码不一致"),
-    "home_page" : MessageLookupByLibrary.simpleMessage("首页"),
-    "input_login_name" : MessageLookupByLibrary.simpleMessage("请输入登录账号"),
-    "input_login_psw" : MessageLookupByLibrary.simpleMessage("请输入登录密码"),
-    "knowledgeHierarchy" : MessageLookupByLibrary.simpleMessage("知识体系"),
-    "leaveTime" : m0,
-    "login" : MessageLookupByLibrary.simpleMessage("登录"),
-    "loginTitle" : MessageLookupByLibrary.simpleMessage("账号登录"),
-    "login_is_not_empty" : MessageLookupByLibrary.simpleMessage("登录账号不可为空"),
-    "login_psw_is_not_empty" : MessageLookupByLibrary.simpleMessage("登录密码不可为空"),
-    "logout" : MessageLookupByLibrary.simpleMessage("退出登录"),
-    "logout1" : MessageLookupByLibrary.simpleMessage("退出登录"),
-    "logout2" : MessageLookupByLibrary.simpleMessage("退出登录"),
-    "logout3" : MessageLookupByLibrary.simpleMessage("退出登录"),
-    "logout4" : MessageLookupByLibrary.simpleMessage("退出登录"),
-    "logout5" : MessageLookupByLibrary.simpleMessage("退出登录"),
-    "logout6" : MessageLookupByLibrary.simpleMessage("退出登录"),
-    "name" : MessageLookupByLibrary.simpleMessage("name"),
-    "officialAccounts" : MessageLookupByLibrary.simpleMessage("公众号"),
-    "pageHomeWelcome" : m1,
-    "please_input_search_msg" : MessageLookupByLibrary.simpleMessage("请输入搜索信息"),
-    "project" : MessageLookupByLibrary.simpleMessage("项目"),
-    "psw" : MessageLookupByLibrary.simpleMessage("密码"),
-    "register" : MessageLookupByLibrary.simpleMessage("注册"),
-    "test" : m2,
-    "true_login_name" : MessageLookupByLibrary.simpleMessage("请输入正确格式的手机号码"),
-    "true_login_psw" : MessageLookupByLibrary.simpleMessage("请输入6-16数字或字母组合的密码")
-  };
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "account": MessageLookupByLibrary.simpleMessage("账号"),
+        "age": MessageLookupByLibrary.simpleMessage("age"),
+        "ensure_psw": MessageLookupByLibrary.simpleMessage("确认密码"),
+        "ensure_psw_true": MessageLookupByLibrary.simpleMessage("密码和确认密码不一致"),
+        "home_page": MessageLookupByLibrary.simpleMessage("首页"),
+        "input_login_name": MessageLookupByLibrary.simpleMessage("请输入登录账号"),
+        "input_login_psw": MessageLookupByLibrary.simpleMessage("请输入登录密码"),
+        "knowledgeHierarchy": MessageLookupByLibrary.simpleMessage("知识体系"),
+        "leaveTime": m0,
+        "login": MessageLookupByLibrary.simpleMessage("登录"),
+        "loginTitle": MessageLookupByLibrary.simpleMessage("账号登录"),
+        "login_is_not_empty": MessageLookupByLibrary.simpleMessage("登录账号不可为空"),
+        "login_psw_is_not_empty":
+            MessageLookupByLibrary.simpleMessage("登录密码不可为空"),
+        "logout": MessageLookupByLibrary.simpleMessage("退出登录"),
+        "logout1": MessageLookupByLibrary.simpleMessage("退出登录"),
+        "logout2": MessageLookupByLibrary.simpleMessage("退出登录"),
+        "logout3": MessageLookupByLibrary.simpleMessage("退出登录"),
+        "logout4": MessageLookupByLibrary.simpleMessage("退出登录"),
+        "logout5": MessageLookupByLibrary.simpleMessage("退出登录"),
+        "logout6": MessageLookupByLibrary.simpleMessage("退出登录"),
+        "name": MessageLookupByLibrary.simpleMessage("name"),
+        "officialAccounts": MessageLookupByLibrary.simpleMessage("公众号"),
+        "pageHomeWelcome": m1,
+        "please_input_search_msg":
+            MessageLookupByLibrary.simpleMessage("请输入搜索信息"),
+        "project": MessageLookupByLibrary.simpleMessage("项目"),
+        "psw": MessageLookupByLibrary.simpleMessage("密码"),
+        "register": MessageLookupByLibrary.simpleMessage("注册"),
+        "test": m2,
+        "true_login_name": MessageLookupByLibrary.simpleMessage("请输入正确格式的手机号码"),
+        "true_login_psw":
+            MessageLookupByLibrary.simpleMessage("请输入6-16数字或字母组合的密码")
+      };
 }
